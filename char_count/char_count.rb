3a416dcdb2987d893067f2580387e589176e0973
mddenton <geositta@gmail.com>
2014-06-29 20:49:48 -0500

    add char_count exercise

---
+++ char_count/char_count.rb
@@ -0,0 +1,26 @@
+
+def char_count(str)
+
+  counts = {}
+  str.downcase.each_codepoint do |char|
+
+    counts[char] = 0 unless counts.include?(char)
+
+    counts[char] += 1
+
+  end
+
+  letter_counts = counts.group_by{ |k, v| v }
+  max_char_count = letter_counts.max
+  min_char_count = letter_counts.min
+  
+  max_char = max_char_count.last.map{ |r| r[0] }.min.chr('UTF-8')
+  min_char = min_char_count.last.map{ |r| r[0] }.min.chr('UTF-8')
+
+  max_count = counts.fetch(max_char_count.last.map{ |r| r[0] }.min)
+  min_count = counts.fetch(min_char_count.last.map{ |r| r[0] }.min)
+
+  return "#{max_count} '#{max_char}', #{min_count} '#{min_char}'"
+
+end
+
---
+++ char_count/spec/char_spec.rb
@@ -0,0 +1,23 @@
+require './char_count'
+
+describe "char_count" do
+	it "accepts an empty string" do 
+		expect(char_count(" ")).to eq("1 ' ', 1 ' '")
+	end
+	it "accepts a string and outputs most and least characters" do 
+		expect(char_count("supercalifragilisticexpialidocious")).to eq("7 'i', 1 'd'")
+	end
+	it "accepts a string and outputs most and least characters" do 
+		expect(char_count("technicolor dreamcoat")).to eq("3 'c', 1 ' '")
+	end
+	it "downcases capital letters before counting" do 
+		expect(char_count("Hello")).to eq("2 'l', 1 'e'")
+	end
+	it "returns the character with the lowest code point in the event of a tie" do 
+		expect(char_count("aabbbii")).to eq("3 'b', 2 'a'")
+	end
+	it "accepts characters from any language" do 
+		expect(char_count("テクニカラードリームコート")).to eq("3 'ー', 1 'カ'")
+	end
+end
+
